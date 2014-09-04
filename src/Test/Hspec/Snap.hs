5e9acda3ae08aa20aec7013c76011d2dd310475a
Daniel Patterson <dbp@dbpmail.net>
2014-09-03 22:17:52 -0400

    add haddocks

--- src/Test/Hspec/Snap.hs
+++ src/Test/Hspec/Snap.hs
@@ -6,15 +6,14 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module Test.Hspec.Snap (
-  -- * Types
-    TestResponse(..)
-  , SnapHspecState(..)
+  -- * Running blocks of hspec-snap tests
+    snap
+
+  -- * Core data types
+  , TestResponse(..)
   , SnapHspecM
 
-  -- * Running tests
-  , snap
-
-  -- * General helpers
+  -- * General Hspec helpers
   , afterAll
 
   -- * Requests
@@ -23,7 +22,7 @@ module Test.Hspec.Snap (
   , post
   , params
 
-  -- * Evaluating handlers
+  -- * Evaluating application code
   , eval
 
   -- * Unit test assertions
@@ -51,7 +50,8 @@ module Test.Hspec.Snap (
   , form
 
 
-  -- * Internal helpers (not guaranteed to remain present in API)
+  -- * Internal types and helpers
+  , SnapHspecState(..)
   , setResult
   , runRequest
   , runHandlerSafe
@@ -84,11 +84,19 @@ import qualified Text.Digestive          as DF
 import qualified Text.HandsomeSoup       as HS
 import qualified Text.XML.HXT.Core       as HXT
 
+-- | The result of making requests against your application. Most
+-- assertions act against these types (for example, `should200`,
+-- `shouldHaveSelector`, etc).
 data TestResponse = Html Text | NotFound | Redirect Int Text | Other Int | Empty deriving (Show, Eq)
 
+-- | The main monad that tests run inside of. This allows both access
+-- to the application (via requests and `eval`) and to running
+-- assertions (like `should404` or `shouldHaveText`).
+type SnapHspecM b = StateT (SnapHspecState b) IO
+
+-- | Internal state used to share site initialization across tests, and to propogate failures.
 data SnapHspecState b = SnapHspecState Result (Handler b b ()) (Snaplet b) (InitializerState b)
 
-type SnapHspecM b = StateT (SnapHspecState b) IO
 
 instance Example (SnapHspecM b ()) where
   type Arg (SnapHspecM b ()) = SnapHspecState b
@@ -98,6 +106,7 @@ instance Example (SnapHspecM b ()) where
                                                putMVar mv r'
        takeMVar mv
 
+-- | Runs a given action once after all the tests in the given block have run.
 afterAll :: IO () -> SpecWith a -> SpecWith a
 afterAll action = go
   where go spec = do forest <- runIO $ runSpecM spec
@@ -120,6 +129,13 @@ afterAll action = go
                                               then action >>= return . (v,)
                                               else return (v - 1, ())
 
+-- | The way to run a block of `SnapHspecM` tests within an `hspec`
+-- test suite. This takes both the top level handler (usually `route
+-- routes`, where `routes` are all the routes for your site) and the
+-- site initializer (often named `app`), and a block of tests. A test
+-- suite can have multiple calls to `snap`, though each one will cause
+-- the site initializer to run, which is often a slow operation (and
+-- will slow down test suites).
 snap :: Handler b b () -> SnapletInit b b -> SpecWith (SnapHspecState b) -> Spec
 snap site app spec = do
   snapinit <- runIO $ getSnaplet (Just "test") app
@@ -129,9 +145,11 @@ snap site app spec = do
       afterAll (closeSnaplet initstate) $
         before (return (SnapHspecState Success site snaplet initstate)) spec
 
+-- | Runs a GET request.
 get :: Text -> SnapHspecM b TestResponse
 get path = get' path M.empty
 
+-- | Runs a GET request, with a set of parameters.
 get' :: Text -> Snap.Params -> SnapHspecM b TestResponse
 get' path ps = runRequest (Test.get (T.encodeUtf8 path) ps)
 
@@ -140,20 +158,25 @@ params :: [(ByteString, ByteString)] -- ^ Pairs of parameter and value.
        -> Snap.Params
 params = M.fromList . map (\x -> (fst x, [snd x]))
 
+-- | Creates a new POST request, with a set of parameters.
 post :: Text -> Snap.Params -> SnapHspecM b TestResponse
 post path ps = runRequest (Test.postUrlEncoded (T.encodeUtf8 path) ps)
 
+-- | Runs an arbitrary stateful action from your application.
 eval :: Handler b b a -> SnapHspecM b a
 eval act = do (SnapHspecState _ _ app is) <- S.get
               liftIO $ fmap (either (error . T.unpack) id) $ evalHandlerSafe act app is
 
 
+-- | Records a test Success or Fail. Only the first Fail will be
+-- recorded (and will cause the whole block to Fail).
 setResult :: Result -> SnapHspecM b ()
 setResult r = do (SnapHspecState r' s a i) <- S.get
                  case r' of
                    Success -> S.put (SnapHspecState r s a i)
                    _ -> return ()
 
+-- | Asserts that two values are equal.
 shouldEqual :: (Show a, Eq a)
             => a
             -> a
@@ -162,6 +185,7 @@ shouldEqual a b = if a == b
                       then setResult Success
                       else setResult (Fail ("Should have held: " ++ show a ++ " == " ++ show b))
 
+-- | Asserts that two values are not equal.
 shouldNotEqual :: (Show a, Eq a)
                => a
                -> a
@@ -170,50 +194,64 @@ shouldNotEqual a b = if a == b
                          then setResult (Fail ("Should not have held: " ++ show a ++ " == " ++ show b))
                          else setResult Success
 
+-- | Asserts that the value is True.
 shouldBeTrue :: Bool
              -> SnapHspecM b ()
 shouldBeTrue True = setResult Success
 shouldBeTrue False = setResult (Fail "Value should have been True.")
 
+-- | Asserts that the value is not True (otherwise known as False).
 shouldNotBeTrue :: Bool
                  -> SnapHspecM b ()
 shouldNotBeTrue False = setResult Success
 shouldNotBeTrue True = setResult (Fail "Value should have been True.")
 
+-- | Asserts that the response is a success (either Html, or Other with status 200).
 should200 :: TestResponse -> SnapHspecM b ()
 should200 (Html _) = setResult Success
 should200 (Other 200) = setResult Success
 should200 r = setResult (Fail (show r))
 
+-- | Asserts that the response is not a normal 200.
 shouldNot200 :: TestResponse -> SnapHspecM b ()
 shouldNot200 (Html _) = setResult (Fail "Got Html back.")
 shouldNot200 (Other 200) = setResult (Fail "Got Other with 200 back.")
 shouldNot200 _ = setResult Success
 
+-- | Asserts that the response is a NotFound.
 should404 :: TestResponse -> SnapHspecM b ()
 should404 NotFound = setResult Success
 should404 r = setResult (Fail (show r))
 
+-- | Asserts that the response is not a NotFound.
 shouldNot404 :: TestResponse -> SnapHspecM b ()
 shouldNot404 NotFound = setResult (Fail "Got NotFound back.")
 shouldNot404 _ = setResult Success
 
+-- | Asserts that the response is a redirect.
 should300 :: TestResponse -> SnapHspecM b ()
 should300 (Redirect _ _) = setResult Success
 should300 r = setResult (Fail (show r))
 
+-- | Asserts that the response is not a redirect.
 shouldNot300 :: TestResponse -> SnapHspecM b ()
 shouldNot300 (Redirect _ _) = setResult (Fail "Got Redirect back.")
 shouldNot300 _ = setResult Success
 
+-- | Asserts that the response is a redirect, and thet the url it
+-- redirects to starts with the given path.
 should300To :: Text -> TestResponse -> SnapHspecM b ()
 should300To pth (Redirect _ to) | pth `T.isPrefixOf` to = setResult Success
 should300To _ r = setResult (Fail (show r))
 
+-- | Asserts that the response is not a redirect to a given path. Note
+-- that it can still be a redirect for this assertion to succeed, the
+-- path it redirects to just can't start with the given path.
 shouldNot300To :: Text -> TestResponse -> SnapHspecM b ()
 shouldNot300To pth (Redirect _ to) | pth `T.isPrefixOf` to = setResult (Fail "Got Redirect back.")
 shouldNot300To _ _ = setResult Success
 
+-- | Assert that a response (which should be Html) has a given selector.
 shouldHaveSelector :: TestResponse -> Text -> SnapHspecM b ()
 shouldHaveSelector r@(Html body) selector =
   setResult $ if haveSelector' r selector
@@ -222,6 +260,7 @@ shouldHaveSelector r@(Html body) selector =
   where msg = (T.unpack $ T.concat ["Html should have contained selector: ", selector, "\n\n", body])
 shouldHaveSelector _ match = setResult (Fail (T.unpack $ T.concat ["Non-HTML body should have contained css selector: ", match]))
 
+-- | Assert that a response (which should be Html) doesn't have a given selector.
 shouldNotHaveSelector :: TestResponse -> Text -> SnapHspecM b ()
 shouldNotHaveSelector r@(Html body) selector =
   setResult $ if haveSelector' r selector
@@ -237,6 +276,7 @@ haveSelector' (Html body) selector =
     _ -> True
 haveSelector' _ _ = False
 
+-- | Asserts that the response (which should be Html) contains the given text.
 shouldHaveText :: TestResponse -> Text -> SnapHspecM b ()
 shouldHaveText (Html body) match =
   if T.isInfixOf match body
@@ -244,6 +284,7 @@ shouldHaveText (Html body) match =
   else setResult (Fail $ T.unpack $ T.concat [body, "' contains '", match, "'."])
 shouldHaveText _ match = setResult (Fail (T.unpack $ T.concat ["Body contains: ", match]))
 
+-- | Asserts that the response (which should be Html) does not contain the given text.
 shouldNotHaveText :: TestResponse -> Text -> SnapHspecM b ()
 shouldNotHaveText (Html body) match =
   if T.isInfixOf match body
@@ -256,7 +297,7 @@ shouldNotHaveText _ _ = setResult Success
 data FormExpectations a = Value a           -- ^ The value the form should take (and should be valid)
                         | ErrorPaths [Text] -- ^ The error paths that should be populated
 
--- | Test against digestive-functors forms.
+-- | Tests against digestive-functors forms.
 form :: (Eq a, Show a)
      => FormExpectations a           -- ^ If the form should succeed, Value a is what it should produce.
                                      --   If failing, ErrorPaths should be all the errors that are triggered.
@@ -276,8 +317,7 @@ form expected theForm theParams =
                             Just v -> return [DF.TextInput v]
         fixedParams = M.mapKeys (T.append "form.") theParams
 
--- Internal helpers
-
+-- | Runs a request (built with helpers from Snap.Test), resulting in a response.
 runRequest :: RequestBuilder IO () -> SnapHspecM b TestResponse
 runRequest req = do
   (SnapHspecState _ site app is) <- S.get
@@ -296,9 +336,8 @@ runRequest req = do
                         return (Redirect (rspStatus response) (T.decodeUtf8 url))
                 else return (Other (rspStatus response))
 
-
-
-
+-- | Runs a request against a given handler (often the whole site),
+-- with the given state. Returns any triggered exception, or the response.
 runHandlerSafe :: RequestBuilder IO ()
                -> Handler b b v
                -> Snaplet b
@@ -307,6 +346,8 @@ runHandlerSafe :: RequestBuilder IO ()
 runHandlerSafe req site s is =
   catch (runHandler' s is req site) (\(e::SomeException) -> return $ Left (T.pack $ show e))
 
+-- | Evaluates a given handler with the given state. Returns any
+-- triggered exception, or the value produced.
 evalHandlerSafe :: Handler b b v
                 -> Snaplet b
                 -> InitializerState b
